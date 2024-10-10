882399542d05124fc63d871942ce5470f3065536
stevan-vanderwerf-sonarsource <97156518+stevan-vanderwerf-sonarsource@users.noreply.github.com>
2024-10-10 09:14:51 -0500

    Update badfortune.py

--- badfortune.py
+++ badfortune.py
@@ -30,6 +30,7 @@ LONG_SIZE = struct.calcsize('L')
 is_64_bit = (LONG_SIZE == 8)
 
 print('sdfsdfsdf')
+print('twotwo')
 
 def get(filename):
     "Select a random quotation, using a pregenerated .dat file"
